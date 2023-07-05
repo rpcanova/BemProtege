3a80dc16d3b35a4e76452a60fc1894d384910f84
unknown <rpcanova@hotmail.com>
2023-07-05 19:25:30 -0300

    -

--- enviar_mensagem.php
+++ enviar_mensagem.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $mensagem = $_POST['form_fields_mensagem'];
 
     // Configurações do e-mail
-    $to = 'rafaelpcanova@gmail.com'; // Insira o endereço de e-mail de destino aqui
+    $to = 'bemprotegevendas@outlook.com'; // Insira o endereço de e-mail de destino aqui
     $subject = 'Mensagem';
     $message = "
         <html>
