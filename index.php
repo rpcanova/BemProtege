552022552630ed2a94b7399c43dc566939ad6463
unknown <rpcanova@hotmail.com>
2023-07-11 12:24:00 -0300

    Correção máscara e botão whatsapp

--- index.php
+++ index.php
@@ -461,14 +461,14 @@ include('enviar_mensagem.php');
         $(document).ready(function() {
             $('#form-field-telefone').mask('(00) 90000-0000');
             $('#form-field-telefone2').mask('(00) 90000-0000');
-            $('#form-field-placa').inputmask({
-                mask: 'AAA-0*00',
-                definitions: {
-
+            $('#form-field-placa').mask('AAA-0B00', {
+                translation: {
+                    'A': { pattern: /[A-Za-z]/ },
+                    'B': { pattern: /[A-Za-z0-9]/}
                 }
-            })
+            });
         });
-    </>
+    </script>
 
     <script type='text/javascript' id='chaty-front-end-js-extra'>
         /* <![CDATA[ */
@@ -548,7 +548,7 @@ include('enviar_mensagem.php');
                 "channels": [{
                     "channel": "Whatsapp",
                     "value": "5511996069219",
-                    "hover_text": "WhatsApp",
+                    "hover_text": "AAA",
                     "svg_icon": "<svg width=\"39\" height=\"39\" viewBox=\"0 0 39 39\" fill=\"none\" xmlns=\"http:\/\/www.w3.org\/2000\/svg\"><circle class=\"color-element\" cx=\"19.4395\" cy=\"19.4395\" r=\"19.4395\" fill=\"#49E670\"\/><path d=\"M12.9821 10.1115C12.7029 10.7767 11.5862 11.442 10.7486 11.575C10.1902 11.7081 9.35269 11.8411 6.84003 10.7767C3.48981 9.44628 1.39593 6.25317 1.25634 6.12012C1.11674 5.85403 2.13001e-06 4.39053 2.13001e-06 2.92702C2.13001e-06 1.46351 0.83755 0.665231 1.11673 0.399139C1.39592 0.133046 1.8147 1.01506e-06 2.23348 1.01506e-06C2.37307 1.01506e-06 2.51267 1.01506e-06 2.65226 1.01506e-06C2.93144 1.01506e-06 3.21063 -2.02219e-06 3.35022 0.532183C3.62941 1.19741 4.32736 2.66092 4.32736 2.79397C4.46696 2.92702 4.46696 3.19311 4.32736 3.32616C4.18777 3.59225 4.18777 3.59224 3.90858 3.85834C3.76899 3.99138 3.6294 4.12443 3.48981 4.39052C3.35022 4.52357 3.21063 4.78966 3.35022 5.05576C3.48981 5.32185 4.18777 6.38622 5.16491 7.18449C6.42125 8.24886 7.39839 8.51496 7.81717 8.78105C8.09636 8.91409 8.37554 8.9141 8.65472 8.648C8.93391 8.38191 9.21309 7.98277 9.49228 7.58363C9.77146 7.31754 10.0507 7.1845 10.3298 7.31754C10.609 7.45059 12.2841 8.11582 12.5633 8.38191C12.8425 8.51496 13.1217 8.648 13.1217 8.78105C13.1217 8.78105 13.1217 9.44628 12.9821 10.1115Z\" transform=\"translate(12.9597 12.9597)\" fill=\"#FAFAFA\"\/><path d=\"M0.196998 23.295L0.131434 23.4862L0.323216 23.4223L5.52771 21.6875C7.4273 22.8471 9.47325 23.4274 11.6637 23.4274C18.134 23.4274 23.4274 18.134 23.4274 11.6637C23.4274 5.19344 18.134 -0.1 11.6637 -0.1C5.19344 -0.1 -0.1 5.19344 -0.1 11.6637C-0.1 13.9996 0.624492 16.3352 1.93021 18.2398L0.196998 23.295ZM5.87658 19.8847L5.84025 19.8665L5.80154 19.8788L2.78138 20.8398L3.73978 17.9646L3.75932 17.906L3.71562 17.8623L3.43104 17.5777C2.27704 15.8437 1.55796 13.8245 1.55796 11.6637C1.55796 6.03288 6.03288 1.55796 11.6637 1.55796C17.2945 1.55796 21.7695 6.03288 21.7695 11.6637C21.7695 17.2945 17.2945 21.7695 11.6637 21.7695C9.64222 21.7695 7.76778 21.1921 6.18227 20.039L6.17557 20.0342L6.16817 20.0305L5.87658 19.8847Z\" transform=\"translate(7.7758 7.77582)\" fill=\"white\" stroke=\"white\" stroke-width=\"0.2\"\/><\/svg>",
                     "is_desktop": 1,
                     "is_mobile": 1,
@@ -557,12 +557,12 @@ include('enviar_mensagem.php');
                     "channel_type": "Whatsapp",
                     "custom_image_url": "",
                     "order": "",
-                    "pre_set_message": "",
+                    "pre_set_message": "Olá, quero fazer uma cotação",
                     "is_use_web_version": "1",
                     "is_open_new_tab": "1",
                     "is_default_open": "0",
                     "has_welcome_message": "0",
-                    "chat_welcome_message": "<p>How can I help you? :)<\/p>",
+                    "chat_welcome_message": "<p>Olá, quero fazer uma cotação<\/p>",
                     "qr_code_image_url": "",
                     "mail_subject": "",
                     "channel_account_type": "personal",
@@ -1924,87 +1924,6 @@ include('enviar_mensagem.php');
             'text direction\u0004ltr': ['ltr']
         });
     </script>
-
-    <!-- <script type='text/javascript' id='elementor-pro-frontend-js-before'>
-        var ElementorProFrontendConfig = {
-            "urls": {
-                "assets": "https:\/\/bemprotegebahia.com.br\/wp-content\/plugins\/elementor-pro\/assets\/",
-                "rest": "https:\/\/bemprotegebahia.com.br\/wp-json\/"
-            }
-            "shareButtonsNetworks": {
-                "facebook": {
-                    "title": "Facebook",
-                    "has_counter": true
-                },
-                "twitter": {
-                    "title": "Twitter"
-                },
-                "linkedin": {
-                    "title": "LinkedIn",
-                    "has_counter": true
-                },
-                "pinterest": {
-                    "title": "Pinterest",
-                    "has_counter": true
-                },
-                "reddit": {
-                    "title": "Reddit",
-                    "has_counter": true
-                },
-                "vk": {
-                    "title": "VK",
-                    "has_counter": true
-                },
-                "odnoklassniki": {
-                    "title": "OK",
-                    "has_counter": true
-                },
-                "tumblr": {
-                    "title": "Tumblr"
-                },
-                "digg": {
-                    "title": "Digg"
-                },
-                "skype": {
-                    "title": "Skype"
-                },
-                "stumbleupon": {
-                    "title": "StumbleUpon",
-                    "has_counter": true
-                },
-                "mix": {
-                    "title": "Mix"
-                },
-                "telegram": {
-                    "title": "Telegram"
-                },
-                "pocket": {
-                    "title": "Pocket",
-                    "has_counter": true
-                },
-                "xing": {
-                    "title": "XING",
-                    "has_counter": true
-                },
-                "whatsapp": {
-                    "title": "WhatsApp"
-                },
-                "email": {
-                    "title": "Email"
-                },
-                "print": {
-                    "title": "Print"
-                }
-            },
-            "facebook_sdk": {
-                "lang": "pt_BR",
-                "app_id": ""
-            },
-            "lottie": {
-                "defaultAnimationUrl": "https:\/\/bemprotegebahia.com.br\/wp-content\/plugins\/elementor-pro\/modules\/lottie\/assets\/animations\/default.json"
-            }
-        };
-    </script> -->
 
     <script type='text/javascript' src='https://bemprotegebahia.com.br/wp-content/plugins/elementor-pro/assets/js/frontend.min.js?ver=3.6.5' id='elementor-pro-frontend-js'></script>
     <script type='text/javascript' src='https://bemprotegebahia.com.br/wp-content/plugins/elementor/assets/lib/waypoints/waypoints.min.js?ver=4.0.2' id='elementor-waypoints-js'></script>
